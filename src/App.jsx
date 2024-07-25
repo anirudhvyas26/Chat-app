883d6140f673b10f23a97bb7109c86b44bf781e4
Anirudh <anirudhvyas26@gmail.com>
2024-07-26 02:47:25 +0530

    addUser

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import List from "./components/list/List";
 import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
 const App = () => {
-  const user = false;
+  const user = true;
   return (
     <div className="container">
       {
