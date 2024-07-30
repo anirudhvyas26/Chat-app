879bc8d52a995585e004af4d24d288b8aba50543
Anirudh <anirudhvyas26@gmail.com>
2024-07-30 16:39:51 +0530

    error

---
+++ src/components/lib/upload.js
@@ -0,0 +1,30 @@
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { Storage } from "./firebase";
+
+const upload = async (file) => {
+    const date = new date()
+    const storageRef = ref(Storage,`images/${date + file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    return new Promise((resolve, reject) => {
+        uploadTask.on('state_changed', 
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log(`Upload is ${progress}% done`);
+                
+            }, 
+            (error) => {
+                reject(`Something went wrong! ${error.code}`);
+            }, 
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    resolve(downloadURL);
+                }).catch((error) => {
+                    reject(`Failed to get download URL! ${error.code}`);
+                });
+            }
+        );
+    });
+};
+
+export default upload;
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "../lib/firebase";
 import { doc, setDoc } from "firebase/firestore";
+import upload from "../lib/upload";
 
 const Login = () => {
     const [avatar, setAvatar]=useState({
@@ -30,11 +31,13 @@ const Login = () => {
     const {username, email, password} = Object.fromEntries(formData);
 
    try{
-const  res = await createUserWithEmailAndPassword(auth,email,password)
+const  res = await createUserWithEmailAndPassword(auth,email,password);
+const imgUrl =await upload(avatar.file)
     
 await setDoc(doc(db, "users", res.user.uid), {
    username,
   email,
+  avatar:imgUrl,
   id: res.user.uid,
   blocked:[],
 });
