e0bef41fffce77df33f228dbe8af0c0466718222
Anirudh <anirudhvyas26@gmail.com>
2024-07-31 17:39:04 +0530

    issues to be resolved

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import Chat from "./components/chat/Chat";
 //import { ToastContainer } from "react-toastify";
 
-import  Detail from "./components/detail/Detail";
+import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
 import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
@@ -10,35 +10,32 @@ import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./components/lib/firebase";
 import { useUserStore } from "./components/lib/userStore";
 const App = () => {
- // const user = false;
- const {currentUser, isLoading, fetchUserInfo} = useUserStore()
+  const user = true;
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
 
-
-  useEffect(()=>{
-const unsub = onAuthStateChanged(auth, (user)=>{
-fetchUserInfo(user.uid);
-});
-  return()=>{
-    unsub();
-  };
-  },[fetchUserInfo]);
-  console.log(currentUser)
-  if(isLoading) return<div className="loading">Loading...</div>
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user.uid);
+    });
+    return () => {
+      unsub();
+    };
+  }, [fetchUserInfo]);
+  console.log(currentUser);
+  if (isLoading) return <div className="loading">Loading...</div>;
   return (
     <div className="container">
-      {
-        user ? (
-         <>
-          <List/>
-          <Chat/>
-          <Detail/>
-         </> 
-          ) : (
-      <Login/>
-    ) }
-    <Notification/>
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
+      <Notification />
     </div>
   );
 };
 export default App;
-  
